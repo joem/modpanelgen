086adec5e700fc999c6df5d8c761344f880df857
Joe Miragliuolo <sod@llawn.com>
2023-10-21 22:06:57 -0400

    Change double quotes to single quotes

--- lib/panel_generator.rb
+++ lib/panel_generator.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative "panel_generator/version"
+require_relative 'panel_generator/version'
 
 module PanelGenerator
   class Error < StandardError; end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
-require "panel_generator"
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'panel_generator'
 
-require "minitest/autorun"
+require 'minitest/autorun'
