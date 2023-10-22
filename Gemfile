9ebe880c2edcb36fb9c9370d1a1cae21e89a05de
Joe Miragliuolo <sod@llawn.com>
2023-10-22 01:56:17 -0400

    Add minitest-reporters

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ source 'https://rubygems.org'
 # Specify your gem's dependencies in panel_generator.gemspec
 gemspec
 
-gem 'rake', '~> 13.0'
+group :test, :development do
+  gem 'rake', '~> 13.0'
+  gem 'minitest', '~> 5.0'
+  gem 'minitest-reporters'
+end
 
-gem 'minitest', '~> 5.0'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,18 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'panel_generator'
 
 require 'minitest/autorun'
+require 'minitest/autorun'
+# Use minitest-reporters only if installed.
+# https://github.com/minitest-reporters/minitest-reporters
+begin
+  require 'minitest/reporters'
+  # Redgreen-capable version of standard Minitest reporter:
+  reporter_options = {
+    # detailed_skip: false,
+    color: true
+  }
+  Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new(reporter_options)
+rescue LoadError
+end
+
+
