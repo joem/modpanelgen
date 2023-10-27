77d435dab043da9769840cd49bc96c19357ea01b
Joe Miragliuolo <sod@llawn.com>
2023-10-27 10:22:02 -0400

    Restructure CLI a bit

--- lib/modular_panel_gen/cli.rb
+++ lib/modular_panel_gen/cli.rb
@@ -15,32 +15,36 @@ module ModularPanelGen
     end
 
     def parse_args(argv)
-      options = OptionParser.new do |opts|
+      @options_parser = OptionParser.new do |opts|
         opts.banner = "Usage: modular_panel_gen.rb [options] [input_file]"
 
         # opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
         #   options[:verbose] = v
         # end
 
-        opts.on_tail("-h", "--help", "show this message") do
-          puts opts
-          exit
-        end
-
-        opts.on_tail('--version', 'print the version number, then exit') do
-          puts "modular_panel_gen #{ModularPanelGen::VERSION}"
-          exit
-        end
+        opts.on_tail("-h", "--help", "show this message") { show_help }
+        opts.on_tail('--version', 'print the version number, then exit') { show_version }
       end
+      @options_parser.parse!(argv)
 
       # Catch cases on invalid options and remind the user what's valid
-      begin
-        options.parse!(argv)
-      rescue OptionParser::InvalidOption => e
-        puts e
-        puts options
-        exit 1
-      end
+    rescue OptionParser::InvalidOption => e
+      puts e
+      # Don't replace the following with `show_help` since we want the `exit 1`
+      puts @options_parser
+      exit 1
+    end
+
+    private
+
+    def show_help
+      puts @options_parser
+      exit
+    end
+
+    def show_version
+      puts "modular_panel_gen #{ModularPanelGen::VERSION}"
+      exit
     end
   end
 end
