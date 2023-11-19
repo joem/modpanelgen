1107a8f5eb37746987a430072d0a6ff6d94d6288
Joe Miragliuolo <sod@llawn.com>
2023-11-19 01:44:37 -0500

    Fix documentation

--- lib/modpanelgen/parser_plugin.rb
+++ lib/modpanelgen/parser_plugin.rb
@@ -2,15 +2,17 @@
 
 module Modpanelgen
   # The class that all parser plugins inherit from.
+  #
+  # Parser plugins must inherit from this class in order for
+  # Modpanelgen::Parser to be able to find them.
+  #
   class ParserPlugin < BasePlugin
-    # `self.inherited(klass)` and `self.descendants` come from Plugin
+    # `self.inherited(klass)` and `self.descendants` come from BasePlugin
 
-    # # Plugins must define run (or whatever) in their own class.
-    # def run; end
-
-    # def self.plugins
-    #   descendants
-    #   # TODO: Make this also look for relevant installed gems!
-    # end
+    # Parser plugins must define ???????? in their own class in order to be
+    # compatible with modpanelgen.
+    # FIXME: Fill in this documention.
+    # FIXME: Make sure the documentation here shows up in rdoc documents.
+    # (Probably need to move it to the top level class documentation?
   end
 end
