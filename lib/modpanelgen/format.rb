4f6ccf3a866d1762414baff5539fa5a165916560
Joe Miragliuolo <sod@llawn.com>
2023-11-20 10:33:00 -0500

    Fix Format's documentation

--- lib/modpanelgen/format.rb
+++ lib/modpanelgen/format.rb
@@ -1,100 +1,52 @@
 # frozen_string_literal: true
 
 module Modpanelgen
-  # Namespace for format methods for dealing with format plugins.
+  # Namespace for methods for dealing with format plugins.
+  #
+  # Format mixes in the PluginHandler module, whose methods rely on the prefix
+  # constants defined here. Although the methods of Format are defined by the
+  # PluginHandler module, we have chosen to document them here for clarity.
+  #
+  # == What's Here
+  #
+  # First, what's elsewhere. \Class \Format:
+  #
+  # - Extends {module PluginHandler}[rdoc-ref:PluginHandler], gaining the following methods:
+  #   - {#plugins}[rdoc-ref:PluginHandler#plugins]: Returns an array of all
+  #     format plugins, loaded or installed.
+  #   - {#loaded_plugins}[rdoc-ref:PluginHandler#loaded_plugins]: Returns an
+  #     array of all loaded ('require'd) format plugins.
+  #   - {#installed_plugins}[rdoc-ref:PluginHandler#installed_plugins]: Returns
+  #     an array of all installed gems that are format plugins.
+  #   - {#search_plugins}[rdoc-ref:PluginHandler#search_plugins]: Returns a
+  #     format plugin based on provided +name+ if found (searching local,
+  #     installed, and loaded plugins); otherwise returns +nil+.
+  #   - {#search_local_plugins}[rdoc-ref:PluginHandler#search_local_plugins]:
+  #     Returns a format plugin in the cwd based on provided +name+ if found;
+  #     otherwise returns +nil+.
+  #   - {#search_installed_plugins}[rdoc-ref:PluginHandler#search_installed_plugins]:
+  #     Returns a format plugin from the installed gems based on provided
+  #     +name+ if found; otherwise returns +nil+.
+  #   - {#search_loaded_plugins}[rdoc-ref:PluginHandler#search_loaded_plugins]:
+  #     Returns a loaded ('require'd) format plugin based on provided +name+ if
+  #     found; otherwise returns +nil+.
+  #   - {#snake_case}[rdoc-ref:PluginHandler#snake_case]: Returns an
+  #     underscored, lowercase form from the expression in the string, and
+  #     changes '::' to '/' to convert namespaces to paths.
+  #   - {#camelcase}[rdoc-ref:PluginHandler#camelcase]: Returns an
+  #     UpperCamelCase form of the expression in the string, and converts '/'
+  #     to '::' to convert paths to namespaces.
+  #
+  # Here, class \Format provides the following constants (which are used by the
+  # methods from \PluginHandler):
+  #
+  # - PLUGIN_PREFIX: Used for methods that need 'modpanelgen-format-'
+  # - PLUGIN_PREFIX_COLONS: Used for methods that need 'Modpanelgen::FormatPlugin'
   #
   module Format
     extend ::Modpanelgen::PluginHandler
 
     PLUGIN_PREFIX = 'modpanelgen-format-'
     PLUGIN_PREFIX_COLONS = 'Modpanelgen::FormatPlugin'
-
-    # TODO: Add all this as documentation? Or does the 'extend' above bring in
-    # the documentation from there??
-
-    # # Lists all format plugins.
-    # #
-    # def plugins
-
-    # # Lists all loaded ('require'd) format plugins.
-    # #
-    # def loaded_plugins
-
-    # # Lists all installed gems that are format plugins.
-    # #
-    # def installed_plugins
-
-    # # Searches for a format plugin based on provided +name+ and returns a new
-    # # instance of it if found, or nil if none found.
-    # #
-    # # Searches in this order:
-    # # 1. local files
-    # # 2. installed gems
-    # # 3. default/loaded plugins
-    # # TODO: Figure out what it returns. The name of the plugin? An new instance of the class?
-    # #
-    # # The +name+ should be the end of the plugin's name,
-    # # without 'modpanelgen/format/' or 'modpanelgen-format-'.
-    # #
-    # # So for example, if the full name of the plugin is
-    # # 'modpanelgen-format-your_format' then +name+ should be
-    # # 'your_format'.
-    # #
-    # def search_plugins(name)
-
-    # # Searches local files in the cwd for a format plugin based on provided
-    # # +name+ and returns a new instance of it if found, or nil if not found.
-    # #
-    # # The +name+ should be the end of the plugin's name,
-    # # without 'modpanelgen/format/' or 'modpanelgen-format-'.
-    # #
-    # # So for example, if the full name of the plugin is
-    # # 'modpanelgen-format-your_format' then +name+ should be
-    # # 'your_format'.
-    # #
-    # def search_local_plugins(name)
-
-    # # Searches installed gems for a format plugin based on provided +name+ and
-    # # returns a new instance of it if found, or nil if not found.
-    # #
-    # # The +name+ should be the end of the plugin's name,
-    # # without 'modpanelgen/format/' or 'modpanelgen-format-'.
-    # #
-    # # So for example, if the full name of the plugin is
-    # # 'modpanelgen-format-your_format' then +name+ should be
-    # # 'your_format'.
-    # #
-    # def search_installed_plugins(name)
-
-    # # Searches loaded ('require'd) gems for a format plugin based on provided
-    # # +name+ and returns a new instance of it if found.
-    # #
-    # # The +name+ should be the end of the plugin's name,
-    # # without 'modpanelgen/format/' or 'modpanelgen-format-'.
-    # #
-    # # So for example, if the full name of the plugin is
-    # # 'modpanelgen-format-your_format' then +name+ should be
-    # # 'your_format'.
-    # #
-    # def search_loaded_plugins(name)
-
-    # # Makes an underscored, lowercase form from the expression in the string.
-    # # And changes '::' to '/' to convert namespaces to paths.
-    # #
-    # #   snake_case('ActiveModel')         # => "active_model"
-    # #   snake_case('ActiveModel::Errors') # => "active_model/errors"
-    # #
-    # # (This method is adapted from rails/activesupport.)
-    # def snake_case(camel_cased_word)
-
-    # # Converts strings to UpperCamelCase.
-    # # Also converts '/' to '::' which is useful for converting
-    # # paths to namespaces.
-    # #
-    # #   camelcase('active_model')                # => "ActiveModel"
-    # #   camelcase('active_model/errors')         # => "ActiveModel::Errors"
-    # #
-    # # (This method is adapted from rails/activesupport.)
-    # def camelcase(term)
   end
 end
